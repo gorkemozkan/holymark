40d4ca3b9312317b6652fe444bdcead7fb6cfd6b
gorkemozkan <email@gorkemozkan.com>
2023-12-16 00:21:26 +0300

    Refactor editor toolbar

--- components/EditorToolbar.vue
+++ components/EditorToolbar.vue
@@ -1,18 +1,13 @@
-<script setup lang="ts">
-const store = useMarkdownStore();
-</script>
-
-
 <template>
-    <div class="flex justify-between bg-zinc-950 dark:bg-zinc-950 border-b border-zinc-900 px-4 py-2 mb-6">
+    <div class="flex justify-between bg-zinc-950 dark:bg-zinc-950 border-b border-zinc-900 px-4 py-2">
         <div class="flex items-center space-x-2">
             <AsideExplorer side="right">
                 <MarkdownTable :keys="['title']" />
             </AsideExplorer>
-            <MarkdownActions :actions="['create','save', 'update', 'print', 'delete']" :markdown="store.markdown" />
+            <MarkdownActions :actions="['create','save', 'update', 'print', 'delete']"/>
         </div>
         <div class="flex items-center space-x-6">
-            <ThemeSwitch />
+            <ThemeSwitch toggle/>
             <UserOptions />
         </div>
     </div>
